a435f82d7bed634c0a55b997d36c3f5b66578d46
sohan724code <sohanmd1375@gmail.com>
2022-09-11 20:06:24 +0600

    unique object

---
+++ index.js
@@ -0,0 +1,323 @@
+let allData = [
+  {
+    careerName: "Biman Bangladesh",
+    career: "BG",
+    price: "4326",
+  },
+  {
+    careerName: "Biman Bangladesh",
+    career: "BG",
+    price: "4326",
+  },
+  {
+    careerName: "Biman Bangladesh",
+    career: "BG",
+    price: "4326",
+  },
+  {
+    careerName: "Biman Bangladesh",
+    career: "BG",
+    price: "4529",
+  },
+  {
+    careerName: "Biman Bangladesh",
+    career: "BG",
+    price: "4529",
+  },
+  {
+    careerName: "Biman Bangladesh",
+    career: "BG",
+    price: "4529",
+  },
+  {
+    careerName: "USBangla Airlines",
+    career: "BS",
+    price: "5133",
+  },
+  {
+    careerName: "USBangla Airlines",
+    career: "BS",
+    price: "5133",
+  },
+  {
+    careerName: "USBangla Airlines",
+    career: "BS",
+    price: "5133",
+  },
+  {
+    careerName: "USBangla Airlines",
+    career: "BS",
+    price: "5133",
+  },
+  {
+    careerName: "USBangla Airlines",
+    career: "BS",
+    price: "5133",
+  },
+  {
+    careerName: "USBangla Airlines",
+    career: "BS",
+    price: "5133",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5182",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5182",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5182",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5182",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5182",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5182",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5182",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5182",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5368",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5368",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5368",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5368",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5368",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5368",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5368",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "5368",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6116",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6116",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6116",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6116",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6116",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6116",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6116",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6116",
+  },
+  {
+    careerName: "Biman Bangladesh",
+    career: "BG",
+    price: "6533",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6769",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6769",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6769",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6769",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6769",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6769",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6769",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "6769",
+  },
+  {
+    careerName: "Biman Bangladesh",
+    career: "BG",
+    price: "6861",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "7702",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "7702",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "7702",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "7702",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "7702",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "7702",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "7702",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "7702",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "8635",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "8635",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "8635",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "8635",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "8635",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "8635",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "8635",
+  },
+  {
+    careerName: "NOVOAIR",
+    career: "VQ",
+    price: "8635",
+  },
+];
+
+console.log(allData);
+
+let sortedData = allData.sort((prev, curr) => curr.price - prev.price);
+console.log("sortedData", sortedData);
+
+let uniqueAirlineData = [
+  ...new Map(allData.map((item) => [item["career"], item])).values(),
+];
+
+console.log("uniqueAirlineData", uniqueAirlineData);
